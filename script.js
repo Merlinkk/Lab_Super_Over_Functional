41810b4fcf88575827eac773713f69e61c549a04
Merlinkk <anshulkashyap2504@gmail.com>
2023-10-04 17:14:15 +0530

    Final changes

--- script.js
+++ script.js
@@ -18,48 +18,8 @@ var wickets = 0
 var turn = 1
 const possibleOutcomes = [0,1,2,3,4,5,6,'W']
 
-
-strikeButton.onclick = () => {
-    totalBalls++
-    console.log(totalBalls)
-    if(turn == 1){
-        var score = possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)]
-        if (score === 'W'){
-            team1Wickets += 1;
-            $team1Wickets.textContent = team1Wickets;
-            ballsFaced1++
-            document.querySelector(`#team1-superover .ball:nth-child(${ballsFaced1})`).textContent = score;
-            if (ballsFaced1==6 || team1Wickets == 2){turn = 2}
-        }
-        else{
-            team1Score += score;
-            $team1Score.textContent = team1Score
-            ballsFaced1++
-            document.querySelector(`#team1-superover .ball:nth-child(${ballsFaced1})`).textContent = score;
-            if (ballsFaced1==6 || team1Wickets == 2){turn = 2}
-        }
-    }
-    else if (turn==2){
-        var score = possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)]
-        if (score === 'W'){
-            team2Wickets += 1;
-            $team2Wickets.textContent = team2Wickets;
-            ballsFaced2++
-            document.querySelector(`#team2-superover .ball:nth-child(${ballsFaced2})`).textContent = score;
-            if (ballsFaced2==6 || team2Wickets == 2){turn=0}
-            
-        }
-        else{
-            team2Score += score;
-            $team2Score.textContent = team2Score;    
-            ballsFaced2++ 
-            document.querySelector(`#team2-superover .ball:nth-child(${ballsFaced2})`).textContent = score;
-            if (ballsFaced2==6 || team2Wickets == 2){turn=0}
-        }
-    }
-
-    if (totalBalls == 12){
-        if (team1Score>team2Score)
+function gameOver(){
+    if (team1Score>team2Score)
         {
             alert("INDIA WINS")
             console.log("INDIA WINS")
@@ -72,7 +32,48 @@ strikeButton.onclick = () => {
         if (team1Score==team2Score){
             alert("IT IS A TIE")
         }
+}
+
+
+strikeButton.onclick = () => {
+    totalBalls++
+    console.log(totalBalls)
+    if(turn == 1){
+        var score = possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)]
+        if (score === 'W'){
+            team1Wickets += 1;
+            $team1Wickets.textContent = team1Wickets;
+            ballsFaced1++
+            document.querySelector(`#team1-superover .ball:nth-child(${ballsFaced1})`).textContent = score;
+            
+        }
+        else{
+            team1Score += score;
+            $team1Score.textContent = team1Score
+            ballsFaced1++
+            document.querySelector(`#team1-superover .ball:nth-child(${ballsFaced1})`).textContent = score;
+        }
+        if (ballsFaced1==6 || team1Wickets == 2){turn = 2}
     }
+    else if (turn==2){
+        var score = possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)]
+        if (score === 'W'){
+            team2Wickets += 1;
+            $team2Wickets.textContent = team2Wickets;
+            ballsFaced2++
+            document.querySelector(`#team2-superover .ball:nth-child(${ballsFaced2})`).textContent = score;
+            
+        }
+        else{
+            team2Score += score;
+            $team2Score.textContent = team2Score;    
+            ballsFaced2++ 
+            document.querySelector(`#team2-superover .ball:nth-child(${ballsFaced2})`).textContent = score;
+        }
+        if (ballsFaced2==6 || team2Wickets == 2 || team2Score>team1Score){turn=3; gameOver()}
+    }   
+
+    
 }
 
 resetButton.onclick = () => {window.location.reload()}
